a3c39051fa17070116673bde4cf5bff58201162b
Ambre Jaeger <ambre.jaeger@gmail.com>
2024-02-29 06:02:37 +0100

    Details modification computing time assesment

--- Univariate_Branching_process_Agents.jl
+++ Univariate_Branching_process_Agents.jl
@@ -1,16 +1,19 @@
 #Univariate Branching process using Agents.jl
 using Agents 
 using StatsBase
+using Plots
+using BenchmarkTools
+using Statistics
+using DataFrames
 
 @agent HSC NoSpaceAgent begin
     quiescence::Bool # whether the agent is likely to divide True = quiescence
-    g::Int
+    age::Int
 end
 
 @agent hemato_cell NoSpaceAgent begin
     age::Int #Age of the cell since she left HSC in number of time step
     nbr_division::Int #nbr of divisions since the Cell left HSC
-    g::Int
 end
 
 
@@ -19,7 +22,7 @@ function initialize_model(nbr_HSC)
     space = nothing 
     life = ABM(Union{HSC, hemato_cell}, space, scheduler = Schedulers.ByType(false, true, Union{HSC, hemato_cell}); properties)
 
-    first_agents = HSC.(1:nbr_HSC, trues(nbr_HSC), ones(nbr_HSC))
+    first_agents = HSC.(1:nbr_HSC, trues(nbr_HSC), zeros(nbr_HSC))
     for cell in first_agents
         add_agent!(cell, life)
     end
@@ -33,17 +36,17 @@ function division!(cell::hemato_cell, model; division_rate = 0.3)
     if sample(0:1, ProbabilityWeights([(1 - division_rate), division_rate])) == 1
         divisions = cell.nbr_division + 1
 
-        add_agent!(hemato_cell, model, cell.age, divisions, 1)
-        add_agent!(hemato_cell, model, cell.age, divisions, 1)
+        add_agent!(hemato_cell, model, cell.age, divisions)
+        add_agent!(hemato_cell, model, cell.age, divisions)
         remove_agent!(cell, model)
     end
 end
 
-function division!(cell::HSC, model; division_rate = 0.3)
+function division!(cell::HSC, model; division_rate = 0.5)
     if sample(0:1, ProbabilityWeights([(1 - division_rate), division_rate])) == 1
         
-        add_agent!(HSC, model, cell.quiescence, 0)
-        add_agent!(HSC, model, cell.quiescence, 0)
+        add_agent!(HSC, model, cell.quiescence, cell.age)
+        add_agent!(HSC, model, cell.quiescence, cell.age)
         remove_agent!(cell, model)
     end
 end
@@ -59,7 +62,7 @@ end
 
 function cell_transition!(cell::HSC, model, transition_rate)
     if sample(0:1, ProbabilityWeights([1 - transition_rate, transition_rate])) == 1
-        add_agent!(hemato_cell, model, 0, 0, 1)
+        add_agent!(hemato_cell, model, 0, 0)
         remove_agent!(cell, model)
     end
 end
@@ -76,13 +79,15 @@ end
 
 function life_step!(cell::HSC, model)
     
-    if cell.quiescence == true 
-        cell_activation!(cell, 0.5)
-        division!(cell, model; division_rate = 0.3)
-    else 
+    if cell.quiescence == true
+        cell.age += 1 
+        cell_activation!(cell, 0.02)
+    else
         cell_transition!(cell, model, 1)
     end
-
+    if typeof(cell) == HSC
+        division!(cell, model; division_rate = 0.5)
+    end
 end
 # Fonctionne
 
@@ -100,11 +105,24 @@ function life_step!(cell::hemato_cell, model)
 end
 
 
-n_steps = 10
-life = initialize_model(1)
-adata = [:g]
-#hsc(a) = a isa HSC
-#wolf(a) = a isa hemato_cell
-#adata = [(HSC, count), (wolf, count)]
-data, _ = run!(life, life_step!, n_steps; adata)
+using TimerOutputs
 
+
+hsc(a) = typeof(a) == HSC
+hemato(a) = typeof(a) == hemato_cell
+adata = [(hsc, count), (hemato, count)]
+
+to = TimerOutput()
+df = DataFrame(step =[0], count_hsc =[0], count_hemato = [0])
+
+for n_steps in [50]
+    @timeit to ("loop"*string(n_steps)) begin 
+        life = initialize_model(1)
+        data,_ = run!(life, life_step!, n_steps; adata)  
+    end
+    append!(df, data)
+end
+print(df)
+show(to)
+#plot(data.step, [data.count_hemato, data.count_hsc, data.count_hemato + data.count_hsc], labels =["HSC" "hemato" "total"])
+#plot(data.step, [data.count_hsc], labels =["HSC"])
